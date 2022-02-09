f1a0b32f03d0c1277962d53b925e3fafdae495b2
bsutton21 <bsutton21@gmail.com>
2022-02-09 13:22:28 -0600

    initializing flask

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index(): 
+    return "Homepage" # don't put html in the return statement
+
+
+if __name__ == "__main__":
+    app.run(debug=True) # tells it to start the server
