d321a6c9d692118b73ab71423434f310e9fa8daa
bsutton21 <bsutton21@gmail.com>
2022-02-09 13:28:40 -0600

    changes

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index(): 
-    return "Homepage" # don't put html in the return statement
+@app.route('/profile/<name')
+def profile(name): 
+    return render_template("profile.html", ) # don't put html in the return statement
 
 
 if __name__ == "__main__":
