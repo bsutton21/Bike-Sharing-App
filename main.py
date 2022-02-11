d02c5724476bbb3f6d06592d65ca7625dc622a05
bsutton21 <bsutton21@gmail.com>
2022-02-11 15:16:48 -0600

    added some html/css and got login screen working

--- main.py
+++ main.py
@@ -8,16 +8,32 @@ app = Flask(__name__)
 username = "admin"
 password = "admin"
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def index(): 
     return render_template("login.html") # loads the login page
 
-@app.route("/predict", methods="POST", "GET")
+@app.route("/main", methods=["GET", "POST"])
+def login():
+    if request.form["username"] == username and request.form["password"] == password:
+        return render_template("main.html")
+    else:
+        return render_template("login.html")
+
+@app.route("/main")
+def main():
+    return render_template("main.html") # loads the main page
+
+@app.route("/predict", methods=["POST", "GET"])
 def predict():
     if request.method == "POST":
-        return ##### TO DO
-    return
+        return render_template("machine_learning.html") # loads the machine learning page
+    else:
+        print("Invalid Request")
 
+@app.route("/visualizations")
+def visual():
+    return render_template("visuzalizations.html") # loads the visualizations page
 
 if __name__ == "__main__":
     app.run(debug=True) # tells it to start the app/server
