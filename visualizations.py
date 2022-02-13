8358544e8d27e728e7a0c363437bb4cabdfdb8d0
bsutton21 <bsutton21@gmail.com>
2022-02-13 14:17:56 -0600

    finishing up visualizations using plotly dash

--- visualizations.py
+++ visualizations.py
@@ -10,7 +10,7 @@ plt.style.use('ggplot');
 
 data.modify_data_visualizations
 bike_visual = pd.read_csv("data/BikeDataVisualizations.csv")
-bike = pd.read_csv("data/BikeData.csv")
+bike = pd.read_csv("data/prediction_data.csv")
 
 # Creates a bar graph to visualize sales over time
 def sales_over_time():
@@ -110,6 +110,37 @@ def humid_scatter():
                linestyle="--");
     return fig.savefig("images/sales_and_humid.png")
 
+def sales_graph():
+    table = bike_visual
+    table_date = table["Datetime"]
+    table_count = table["Rented Bike Count"]
+    sales_vis = {"date": table_date, "count": table_count}
+    return sales_vis
+
+def temp_graph():
+    table = bike_visual
+    table_temp = table["Temperature (C)"]
+    table_count = table["Rented Bike Count"]
+    sales_vis = {"temperature": table_temp, "count": table_count}
+    return sales_vis
+
+def precip_graph():
+    table = bike_visual
+    table_precip = table["Precipitation"]
+    table_count = table["Rented Bike Count"]
+    table_date = table["Date"]
+    sales_vis = {"precipitation": table_precip, "count": table_count, "date": table_date}
+    return sales_vis
+
+def humid_graph():
+    table = bike_visual
+    table_humid = table["Humidity (%)"]
+    table_count = table["Rented Bike Count"]
+    sales_vis = {"humidity": table_humid, "count": table_count}
+    return sales_vis
+
 def bike_head():
-    bike_head = bike.head(25)
-    return bike_head.to_csv("data/bike_head.csv")
+    bike_head = bike.head(30)
+    table_rows = bike_head
+    table = {"columns": bike.columns, "rows": table_rows}
+    return table
